3978a5eb6f50e531ea21e79bbbd2367f3be11ff6
Priyansh Thakar <priyanshthakar@gmail.com>
2023-07-27 19:38:55 -0400

    Complete nytimes

--- js/nytimes.js
+++ js/nytimes.js
@@ -1,7 +1,7 @@
 // The URL for the Article Search API at nytimes.com
 const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 // STEP 1: Get your own API key and paste it below…
-const key = '';
+const key = 'ajHuiZdBc3J89usJJyWocrCGnSFPDfPW';
 let url;
 // Grab references to all the DOM elements you'll need to manipulate
 const searchTerm = document.querySelector('.search');
@@ -10,31 +10,41 @@ const endDate = document.querySelector('.end-date');
 const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
 const section = document.querySelector('section');
-// STEP 2: Add a submit event listener for the search form, referencing the fetchResults function as the callback
-
+// STEP 2: Add a submit event listener for the search form, 
+//referencing the fetchResults function as the callback
+submitBtn.addEventListener("click", fetchResults);
 
 // Functions
 function fetchResults(event) {
     // Use preventDefault() to stop the form submitting
     event.preventDefault();
     // STEP 3: Assemble the full URL, according to the API documentation at the New York Times
+    url = `${baseURL}?api-key=${key}&q=${searchTerm.value}`;
+    if (startDate.value !== "") {
+        url += "&begin_date=" + startDate.value; //url = url + "&begin_date=" + startDate.value;
+    }
+    if (endDate.value !== "") {
+        url += "&end_date=" + endDate.value;
+    }
+    console.log(url);
 
-
-    // STEP 4: Use fetch() to pass the URL that we built as a request to the API service, then pass the JSON to the displayResults() function
-
-
+    // STEP 4: Use fetch() to pass the URL that we built as a request to the API service, 
+    //then pass the JSON to the displayResults() function
+    fetch(url).then(response => {
+        return response.json();
+    }).then(json => displayResults(json))
 };
 
 function displayResults(json) {
     // STEP 5: Log to the console the results from the API
-
+    console.log(json);
 
     // Clear out the old results…
     while (section.firstChild) {
         section.removeChild(section.firstChild);
     };
     // STEP 6: Create the variable articles to capture the articles from the JSON object
-    let articles;
+    let articles = json.response.docs;
 
     if (articles.length === 0) {
         const para = document.createElement('p');
